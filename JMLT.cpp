398cf087067a02766edccdd6c76286c863ccbdc4
JST max! <87650746+MaxWasTakenYT@users.noreply.github.com>
2024-09-11 18:27:38 +0000

    Update JMLT.cpp

--- JMLT.cpp
+++ JMLT.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+
 #define say return
 #define what 0
 
@@ -16,6 +17,7 @@ int main(int argc, char* argv[]) {
 
     if (argc > 1) {
         string option = argv[1];
+
         if (option == "-ju" || option == "--prepare-for-jstmax-use") {
             cout << withroot;
             cout << "prepare for \"jstmax!-use\"? (y/n): ";
@@ -35,7 +37,6 @@ int main(int argc, char* argv[]) {
                         else {
                             cout << err; say what;
                         }
-                        cout << ready4use << endl; say what;
                     }
                     else {
                         cout << ready4use << endl; say what;
@@ -50,10 +51,9 @@ int main(int argc, char* argv[]) {
                 cout << "aborting.." << endl; say what;
             }
         }
-
         else if (argc > 2 && (string(argv[2]) == "-c" || string(argv[2]) == "--custom-command")) {
             cout << "enter custom command: ";
-            string customCmd; cin >> customCmd;
+            string customCmd; cin.ignore(); getline(cin, customCmd);
             int returnCustomCmd = system(customCmd.c_str());
             if (returnCustomCmd == 0) {
                 cout << "\ncommand executed correctly!\n";
@@ -63,75 +63,67 @@ int main(int argc, char* argv[]) {
                 cout << err << endl; say what;
             }
         }
-
-        else if (argc > 3 && (string(argv[3]) == "-pmy" || string(argv[3]) == "--pacman-yes")) {
+        else if (argc > 2 && (string(argv[2]) == "-pmy" || string(argv[2]) == "--pacman-yes")) {
             cout << "welcome to \"pacman-yes\" (or jm-pacmany)\n";
             cout << withroot;
             cout << "package name?: ";
             string pkgName; cin >> pkgName;
             cout << "-S, -Sy or -Syu?: ";
             string pmh2d; cin >> pmh2d;
+            string pkg2inst;
             if (pmh2d == "-S" || pmh2d == "-s") {
                 cout << "installing package..";
-                string pkg2inst = "sudo pacman -S " + pkgName;
-                int pmyres = system(pkg2inst.c_str());
-                if (pmyres == 0) { cout << "package installed successfully!"; }
-                else { cout << err << endl; }
-                say what;
+                pkg2inst = "sudo pacman -S " + pkgName;
             }
             else if (pmh2d == "-Sy" || pmh2d == "-sy") {
                 cout << "installing package..";
-                string pkg2inst = "sudo pacman -Sy " + pkgName;
-                int pmyres = system(pkg2inst.c_str());
-                if (pmyres == 0) { cout << "package installed successfully!"; }
-                else { cout << err << endl; }
-                say what;
+                pkg2inst = "sudo pacman -Sy " + pkgName;
             }
             else if (pmh2d == "-Syu" || pmh2d == "-syu") {
                 cout << "installing package..";
-                string pkg2inst = "sudo pacman -Syu " + pkgName;
-                int pmyres = system(pkg2inst.c_str());
-                if (pmyres == 0) { cout << "package installed and full system update ended successfully!"; }
-                else { cout << err << endl; }
+                pkg2inst = "sudo pacman -Syu " + pkgName;
+            }
+            else {
+                cout << "unknown pacman command: " << pmh2d << endl;
                 say what;
             }
+            int pmyres = system(pkg2inst.c_str());
+            if (pmyres == 0) { cout << "package operation completed successfully!"; }
+            else { cout << err << endl; }
+            say what;
         }
-        else if (argc > 4 && (string(argv[4]) == "-yy" || string(argv[4]) == "--yay-yes")) {
+        else if (argc > 2 && (string(argv[2]) == "-yy" || string(argv[2]) == "--yay-yes")) {
             cout << "welcome to \"yay-yes\" (or jm-yayy)\n";
             cout << noroot;
             cout << "package name?: ";
             string pkgName; cin >> pkgName;
             cout << "-S, -Sy or -Syu?: ";
             string pmh2d; cin >> pmh2d;
+            string pkg2inst;
             if (pmh2d == "-S" || pmh2d == "-s") {
                 cout << "installing package..";
-                string pkg2inst = "yay -S " + pkgName;
-                int pmyres = system(pkg2inst.c_str());
-                if (pmyres == 0) { cout << "package installed successfully!"; }
-                else { cout << err << endl; }
-                say what;
+                pkg2inst = "yay -S " + pkgName;
             }
             else if (pmh2d == "-Sy" || pmh2d == "-sy") {
                 cout << "installing package..";
-                string pkg2inst = "sudo yay -Sy " + pkgName;
-                int pmyres = system(pkg2inst.c_str());
-                if (pmyres == 0) { cout << "package installed successfully!"; }
-                else { cout << err << endl; }
-                say what;
+                pkg2inst = "yay -Sy " + pkgName;
             }
             else if (pmh2d == "-Syu" || pmh2d == "-syu") {
                 cout << "installing package..";
-                string pkg2inst = "sudo yay -Syu " + pkgName;
-                int pmyres = system(pkg2inst.c_str());
-                if (pmyres == 0) { cout << "package installed and full system update ended successfully!"; }
-                else { cout << err << endl; }
+                pkg2inst = "yay -Syu " + pkgName;
+            }
+            else {
+                cout << "unknown yay command: " << pmh2d << endl;
                 say what;
             }
+            int pmyres = system(pkg2inst.c_str());
+            if (pmyres == 0) { cout << "package operation completed successfully!"; }
+            else { cout << err << endl; }
+            say what;
         }
-
         else {
             cout << "every option is available on the github repo's readme.md!";
-            cout << "\nunknown option: " << option << endl;
+            cout << "\nunknown option: " << argv[1] << endl;
         }
     }
     say what;
