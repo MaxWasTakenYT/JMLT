d86c7e7a004966371b57be558cf38bc1d95cdf51
JST max! <87650746+MaxWasTakenYT@users.noreply.github.com>
2024-09-11 19:11:06 +0000

    Update JMLT.cpp

--- JMLT.cpp
+++ JMLT.cpp
@@ -7,154 +7,137 @@
 using namespace std;
 
 /*
-READ ME! FOR CODE CONTRIBUTORS:
-To add a new option, you'll need to create a new option string too, here's an example;
-else if (argc > 5) {
-    string optioneo = argv[5];
-    if (optionyy == "-eo" || optionyy == "--example-option") {
-        // And here you'll create the actual code
-    }
-Note: After you created the option, TEST IT! And, if it works, then add a .cpp file in the 'sources' dir with the option's code
-                                                                    then create the pull request as usual and wait for a merge.
+READ, CODE CONTRIBUTORS!:
+After you created the option, TEST IT! And, if it works, add a .cpp file in the 'sources' dir with the option's code.
 */
 
 int main(int argc, char* argv[]) {
     cout << "welcome to jstmax's Linux Toolbox! (JMLT 4 short)\n\n";
 
     string ready4use = "now your system is ready to be \"jstmax!-used\"!";
-    string err = "an error occurred, funny thing is that i don't want or have time to create a way to check logs so.. i guess \"fuck you, try again\" is the right answer this time?\n";
+    string err = "An error occurred. Please try again.\n";
     string withroot = "[!] remember to run as root/with root privileges!!\n\n";
     string noroot = "[!] remember to NOT run as root/with root privileges!!\n\n";
 
     if (argc > 1) {
-        string optionju = argv[1];
-        if (optionju == "-ju" || optionju == "--prepare-for-jstmax-use") {
+        string option = argv[1];
+        if (option == "-ju" || option == "--prepare-for-jstmax-use") {
             cout << withroot;
             cout << "prepare for \"jstmax!-use\"? (y/n): ";
             string prepYoN; cin >> prepYoN;
             if (prepYoN == "y" || prepYoN == "Y") {
                 cout << "\npreparing for \"jstmax!-use\"..\n";
-                system("cd ~/");
                 int returnCode = system("sudo pacman -Sy --noconfirm firefox reflector tldr fastfetch");
                 if (returnCode == 0) {
-                    cout << "run a full system update? (y/n)\n: "; string fsuChoice; cin >> fsuChoice;
+                    cout << "run a full system update? (y/n): ";
+                    string fsuChoice; cin >> fsuChoice;
                     if (fsuChoice == "y" || fsuChoice == "Y") {
                         cout << "running a full system update..\n";
                         int fsuRes = system("sudo pacman -Syu --noconfirm");
                         if (fsuRes == 0) {
-                            cout << ready4use << endl; say what;
+                            cout << ready4use << endl;
                         }
                         else {
-                            cout << err; say what;
+                            cout << err;
                         }
-                        cout << ready4use << endl; say what;
                     }
                     else {
-                        cout << ready4use << endl; say what;
+                        cout << ready4use << endl;
                     }
                 }
                 else {
-                    cout << err << endl;
-                    say what;
+                    cout << err;
                 }
             }
-            else if (argc > 2) {
-                string optionc = argv[2];
-                if (optionc == "-c" || optionc == "--custom-command") {
-                    cout << "enter custom command: ";
-                    string customCmd; cin >> customCmd;
-                    int returnCustomCmd = system(customCmd.c_str());
-                    if (returnCustomCmd == 0) {
-                        cout << "\ncommand executed correctly!\n";
-                        say what;
-                    }
-                    else {
-                        cout << err << endl; say what;
-                    }
-                }
-            }
-            else if (argc > 3) {
-                string optionpmy = argv[3];
-                if (optionpmy == "-pmy" || optionpmy == "--pacman-yes") {
-                    cout << "welcome to \"pacman-yes\" (or jm-pacmany)\n";
-                    cout << withroot;
-                    cout << "package name?: ";
-                    string pkgName; cin >> pkgName;
-                    cout << "-S, -Sy or -Syu?: ";
-                    string pmh2d; cin >> pmh2d;
-                    if (pmh2d == "-S" || pmh2d == "-s") {
-                        cout << "installing package..";
-                        string pkg2inst = "sudo pacman -S " + pkgName;
-                        int pmyres = system(pkg2inst.c_str());
-                        if (pmyres == 0) { cout << "package installed successfully!"; }
-                        else { cout << err << endl; }
-                        say what;
-                    }
-                    else if (pmh2d == "-Sy" || pmh2d == "-sy") {
-                        cout << "installing package..";
-                        string pkg2inst = "sudo pacman -Sy " + pkgName;
-                        int pmyres = system(pkg2inst.c_str());
-                        if (pmyres == 0) { cout << "package installed successfully!"; }
-                        else { cout << err << endl; }
-                        say what;
-                    }
-                    else if (pmh2d == "-Syu" || pmh2d == "-syu") {
-                        cout << "installing package..";
-                        string pkg2inst = "sudo pacman -Syu " + pkgName;
-                        int pmyres = system(pkg2inst.c_str());
-                        if (pmyres == 0) { cout << "package installed and full system update ended successfully!"; }
-                        else { cout << err << endl; }
-                        say what;
-                    }
-                }
-            }
-            else if (argc > 4) {
-                string optionyy = argv[4];
-                if (optionyy == "-yy" || optionyy == "--yay-yes") {
-                    cout << "welcome to \"yay-yes\" (or jm-yayy)\n";
-                    cout << noroot;
-                    cout << "package name?: ";
-                    string pkgName; cin >> pkgName;
-                    cout << "-S, -Sy or -Syu?: ";
-                    string pmh2d; cin >> pmh2d;
-                    if (pmh2d == "-S" || pmh2d == "-s") {
-                        cout << "installing package..";
-                        string pkg2inst = "yay -S " + pkgName;
-                        int pmyres = system(pkg2inst.c_str());
-                        if (pmyres == 0) { cout << "package installed successfully!"; }
-                        else { cout << err << endl; }
-                        say what;
-                    }
-                    else if (pmh2d == "-Sy" || pmh2d == "-sy") {
-                        cout << "installing package..";
-                        string pkg2inst = "sudo yay -Sy " + pkgName;
-                        int pmyres = system(pkg2inst.c_str());
-                        if (pmyres == 0) { cout << "package installed successfully!"; }
-                        else { cout << err << endl; }
-                        say what;
-                    }
-                    else if (pmh2d == "-Syu" || pmh2d == "-syu") {
-                        cout << "installing package..";
-                        string pkg2inst = "sudo yay -Syu " + pkgName;
-                        int pmyres = system(pkg2inst.c_str());
-                        if (pmyres == 0) { cout << "package installed and full system update ended successfully!"; }
-                        else { cout << err << endl; }
-                        say what;
-                    }
-                }
-            }
-
             else {
-                cout << "aborting.." << endl; say what;
+                cout << "aborting..\n";
             }
         }
+        else if (option == "-c" || option == "--custom-command") {
+            if (argc > 2) {
+                cout << "enter custom command: ";
+                string customCmd;
+                cin.ignore();
+                getline(cin, customCmd);
+                int returnCustomCmd = system(customCmd.c_str());
+                if (returnCustomCmd == 0) {
+                    cout << "\ncommand executed correctly!\n";
+                }
+                else {
+                    cout << err;
+                }
+            }
+            else {
+                cout << "no command provided.\n";
+            }
+        }
+        else if (option == "-pmy" || option == "--pacman-yes") {
+            cout << withroot;
+            cout << "package name?: ";
+            string pkgName; cin >> pkgName;
+            cout << "-S, -Sy or -Syu?: ";
+            string pmh2d; cin >> pmh2d;
 
-        
+            string pkg2inst;
+            if (pmh2d == "-S" || pmh2d == "-s") {
+                pkg2inst = "sudo pacman -S " + pkgName;
+            }
+            else if (pmh2d == "-Sy" || pmh2d == "-sy") {
+                pkg2inst = "sudo pacman -Sy " + pkgName;
+            }
+            else if (pmh2d == "-Syu" || pmh2d == "-syu") {
+                pkg2inst = "sudo pacman -Syu " + pkgName;
+            }
+            else {
+                cout << "unknown pacman command: " << pmh2d << endl;
+                say what;
+            }
 
+            int pmyres = system(pkg2inst.c_str());
+            if (pmyres == 0) {
+                cout << "package operation completed successfully!\n";
+            }
+            else {
+                cout << err;
+            }
+        }
+        else if (option == "-yy" || option == "--yay-yes") {
+            cout << noroot;
+            cout << "package name?: ";
+            string pkgName; cin >> pkgName;
+            cout << "-S, -Sy or -Syu?: ";
+            string pmh2d; cin >> pmh2d;
+
+            string pkg2inst;
+            if (pmh2d == "-S" || pmh2d == "-s") {
+                pkg2inst = "yay -S " + pkgName;
+            }
+            else if (pmh2d == "-Sy" || pmh2d == "-sy") {
+                pkg2inst = "yay -Sy " + pkgName;
+            }
+            else if (pmh2d == "-Syu" || pmh2d == "-syu") {
+                pkg2inst = "yay -Syu " + pkgName;
+            }
+            else {
+                cout << "unknown yay command: " << pmh2d << endl;
+                say what;
+            }
+
+            int pmyres = system(pkg2inst.c_str());
+            if (pmyres == 0) {
+                cout << "package operation completed successfully!\n";
+            }
+            else {
+                cout << err;
+            }
+        }
         else {
             cout << "every option is available on the github repo's readme.md!\n";
             cout << "unknown option.\n";
         }
+    }
+    else {
+        cout << "No arguments provided.\n";
     }
     say what;
 }
