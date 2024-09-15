6dee2966bf0b397eec5ece362e76e411f9159dc4
JST max! <87650746+MaxWasTakenYT@users.noreply.github.com>
2024-09-15 19:13:07 +0000

    Update JMLT.cpp

--- JMLT.cpp
+++ JMLT.cpp
@@ -1,143 +1,59 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
-#define say return
-#define what 0
 
 using namespace std;
 
-/*
-READ, CODE CONTRIBUTORS!:
-After you created the option, TEST IT! And, if it works, add a .cpp file in the 'sources' dir with the option's code.
-*/
+int main() {
+    system("clear");
+    system("figlet JMLT");
+    cout << "\nRunning version T4!";
 
-int main(int argc, char* argv[]) {
-    cout << "welcome to jstmax's Linux Toolbox! (JMLT 4 short)\n\n";
+    cout << "\n\nMain menu;";
+    cout << "\n[J] Prepare for \"jstmax! use\"";
+    cout << "\n[C] Enter a custom command";
+    cout << "\n[PMY] Runs pacmany (You need to install it first.)";
+    cout << "\n[YY] Runs yayy (You need to install it first.)";
 
-    string ready4use = "now your system is ready to be \"jstmax!-used\"!";
-    string err = "An error occurred. Please try again.\n";
-    string withroot = "[!] remember to run as root/with root privileges!!\n\n";
-    string noroot = "[!] remember to NOT run as root/with root privileges!!\n\n";
-
-    if (argc > 1) {
-        string option = argv[1];
-        if (option == "-ju" || option == "--prepare-for-jstmax-use") {
-            cout << withroot;
-            cout << "prepare for \"jstmax!-use\"? (y/n): ";
-            string prepYoN; cin >> prepYoN;
-            if (prepYoN == "y" || prepYoN == "Y") {
-                cout << "\npreparing for \"jstmax!-use\"..\n";
-                int returnCode = system("sudo pacman -Sy --noconfirm firefox reflector tldr fastfetch");
-                if (returnCode == 0) {
-                    cout << "run a full system update? (y/n): ";
-                    string fsuChoice; cin >> fsuChoice;
-                    if (fsuChoice == "y" || fsuChoice == "Y") {
-                        cout << "running a full system update..\n";
-                        int fsuRes = system("sudo pacman -Syu --noconfirm");
-                        if (fsuRes == 0) {
-                            cout << ready4use << endl;
-                        }
-                        else {
-                            cout << err;
-                        }
-                    }
-                    else {
-                        cout << ready4use << endl;
-                    }
-                }
-                else {
-                    cout << err;
-                }
+    cout << "\n\nEnter a selection: "; string menuSel; cin >> menuSel;
+    if (menuSel == "J" || menuSel == "j") {
+        system("clear");
+        system("figlet \"JM-Use\"");
+        cout << "\ncontinue?\n";
+        string juA; cin >> juA;
+        if (juA == "y" || juA == "Y") {
+            system("sudo pacman -S --noconfirm figlet tldr fish reflector fastfetch");
+            cout << "\nRun a full system upgrade?\n";
+            string doFsu; cin >> doFsu;
+            if (doFsu == "y" || doFsu == "Y") {
+                system("sudo pacman -Syu --noconfirm");
+                return 0;
             }
             else {
-                cout << "aborting..\n";
-            }
-        }
-        else if (option == "-c" || option == "--custom-command") {
-            if (argc > 2) {
-                cout << "enter custom command: ";
-                string customCmd;
-                cin.ignore();
-                getline(cin, customCmd);
-                int returnCustomCmd = system(customCmd.c_str());
-                if (returnCustomCmd == 0) {
-                    cout << "\ncommand executed correctly!\n";
-                }
-                else {
-                    cout << err;
-                }
-            }
-            else {
-                cout << "no command provided.\n";
-            }
-        }
-        else if (option == "-pmy" || option == "--pacman-yes") {
-            cout << withroot;
-            cout << "package name?: ";
-            string pkgName; cin >> pkgName;
-            cout << "-S, -Sy or -Syu?: ";
-            string pmh2d; cin >> pmh2d;
-
-            string pkg2inst;
-            if (pmh2d == "-S" || pmh2d == "-s") {
-                pkg2inst = "sudo pacman -S " + pkgName;
-            }
-            else if (pmh2d == "-Sy" || pmh2d == "-sy") {
-                pkg2inst = "sudo pacman -Sy " + pkgName;
-            }
-            else if (pmh2d == "-Syu" || pmh2d == "-syu") {
-                pkg2inst = "sudo pacman -Syu " + pkgName;
-            }
-            else {
-                cout << "unknown pacman command: " << pmh2d << endl;
-                say what;
-            }
-
-            int pmyres = system(pkg2inst.c_str());
-            if (pmyres == 0) {
-                cout << "package operation completed successfully!\n";
-            }
-            else {
-                cout << err;
-            }
-        }
-        else if (option == "-yy" || option == "--yay-yes") {
-            cout << noroot;
-            cout << "package name?: ";
-            string pkgName; cin >> pkgName;
-            cout << "-S, -Sy or -Syu?: ";
-            string pmh2d; cin >> pmh2d;
-
-            string pkg2inst;
-            if (pmh2d == "-S" || pmh2d == "-s") {
-                pkg2inst = "yay -S " + pkgName;
-            }
-            else if (pmh2d == "-Sy" || pmh2d == "-sy") {
-                pkg2inst = "yay -Sy " + pkgName;
-            }
-            else if (pmh2d == "-Syu" || pmh2d == "-syu") {
-                pkg2inst = "yay -Syu " + pkgName;
-            }
-            else {
-                cout << "unknown yay command: " << pmh2d << endl;
-                say what;
-            }
-
-            int pmyres = system(pkg2inst.c_str());
-            if (pmyres == 0) {
-                cout << "package operation completed successfully!\n";
-            }
-            else {
-                cout << err;
+                return 0;
             }
         }
         else {
-            cout << "every option is available on the github repo's readme.md!\n";
-            cout << "unknown option.\n";
+            return 0;
         }
+
+    }
+    else if (menuSel == "C" || menuSel == "c") {
+        system("clear");
+        system("figlet \"jC.CMD\"");
+        cout << "~> "; string jccmd; cin >> jccmd;
+        system(jccmd.c_str());
+    }
+    else if (menuSel == "PMY" || menuSel == "pmy") {
+        system("clear");
+        system("pacmany"); return 0;
+    }
+    else if (menuSel == "YY" || menuSel == "yy") {
+        system("clear");
+        system("yayy"); return 0;
     }
     else {
-        cout << "No arguments provided.\n";
+        system("clear");
+        cout << "Unknown option, try again.\n"; return 0;
     }
-    say what;
 }
