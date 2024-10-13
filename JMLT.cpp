4a733fff33a2de719f38e56cf5398592099cad25
jstmax! <87650746+MaxWasTakenYT@users.noreply.github.com>
2024-10-13 18:01:32 +0000

    JMLT T7b2

--- JMLT.cpp
+++ JMLT.cpp
@@ -19,7 +19,6 @@ string menu() {
 }
 
 int main() {
-	menu();
     system("clear"); // Clears the screen for good measure
 	
 	string menuSel = menu(); // Getting input from user	for menu selection
@@ -35,36 +34,31 @@ int main() {
             system("sudo pacman -Syu --noconfirm figlet tldr fish reflector fastfetch lolcat");
             // TODO: Add packages with yay (e.g. rustdesk)
 			}
-			break;
+			main();
 		case 'c':
-		// Probably gonna remove this, pretty useless and bad overall for security
+		// Probably gonna remove this, pretty useless and overall bad for security
 			system("clear");
         	system("figlet \"jC.CMD\"");
         	cout << "~> ";
         	getline(cin, jccmd);
         	system(jccmd.c_str());
-			break;
 		case 'p':
 		// Exits JMLT and runs pacmany
 			system("clear");
         	system("pacmany"); 
-        	return 0;
 			break;
 		case 'y':
 		// Exits JMLT and runs yayy
 			system("clear");
         	system("yayy"); 
-        	return 0;
 			break;
 		case 'q':
 		// Exits JMLT
-			return 0;
 			break;
 		default:
 		// Goes to menu() if user enters an invalid input
         	system("clear");
         	cout << "Unknown option, try again.\n"; 
         	main();
-			break;
 	}
 }
