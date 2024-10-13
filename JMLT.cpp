690097cfb68d77b1f19fe5a4e50572347a5dece7
jstmax! <87650746+MaxWasTakenYT@users.noreply.github.com>
2024-10-13 17:57:17 +0000

    JMLT T7b

--- JMLT.cpp
+++ JMLT.cpp
@@ -3,60 +3,68 @@
 #include <string>
 using namespace std;
 
-int main() {
-    system("clear");
-
-mainMenu:
-    system("figlet JMLT");
-    cout << "\nRunning version T6!";
+string menu() {
+	system("figlet JMLT");
+    cout << "\nRunning version T7b!";
     cout << "\n\nMain menu;";
-    cout << "\n[J] Prepare for \"jstmax! use\"";
-    cout << "\n[C] Enter a custom command";
-    cout << "\n[PMY] Runs pacmany";
-    cout << "\n[YY] Runs yayy";
-    cout << "\n[Q] Quits JMLT";
+    cout << "\n[j] Prepare for \"jstmax! use\"";
+    cout << "\n[c] Enter a custom command";
+    cout << "\n[p] Runs pacmany";
+    cout << "\n[y] Runs yayy";
+    cout << "\n[q] Quits JMLT";
     cout << "\n\nEnter a selection: ";
-    string menuSel; cin >> menuSel;
+    string menuSel;
+	cin >> menuSel;
+	return menuSel;
+}
 
-    if (menuSel == "J" || menuSel == "j") {
-        system("clear");
-        system("figlet \"JM-Use\"");
-        cout << "\ncontinue?\n";
-        string juA; cin >> juA;
-        if (juA == "y" || juA == "Y") {
-            system("sudo pacman -Syu --noconfirm figlet tldr fish reflector fastfetch");
-            // add packages with yay (e.g. rustdesk)
-            goto mainMenu;
-        } else {
-            goto mainMenu;
-        }
-    } 
-    else if (menuSel == "C" || menuSel == "c") {
-        system("clear");
-        system("figlet \"jC.CMD\"");
-        cout << "~> "; 
-        string jccmd; 
-        getline(cin, jccmd);
-        system(jccmd.c_str());
-        goto mainMenu;
-    } 
-    else if (menuSel == "PMY" || menuSel == "pmy") {
-        system("clear");
-        system("pacmany"); 
-        return 0;
-    } 
-    else if (menuSel == "YY" || menuSel == "yy") {
-        system("clear");
-        system("yayy"); 
-        return 0;
-    } 
-    else if (menuSel == "Q" || menuSel == "q") {
-        system("clear");
-        return 0;
-    } 
-    else {
-        system("clear");
-        cout << "Unknown option, try again.\n"; 
-        goto mainMenu;
-    }
+int main() {
+	menu();
+    system("clear"); // Clears the screen for good measure
+	
+	string menuSel = menu(); // Getting input from user	for menu selection
+	// Declaring variables (to get input inside the switch)
+	string juA; string jccmd;
+	switch (menuSel[0]) {
+		case 'j':
+			system("clear");
+        	system("figlet \"JM-Use\"");
+        	cout << "\nContinue?\n";
+        	cin >> juA;
+        	if (juA == "y" || juA == "Y") {
+            system("sudo pacman -Syu --noconfirm figlet tldr fish reflector fastfetch lolcat");
+            // TODO: Add packages with yay (e.g. rustdesk)
+			}
+			break;
+		case 'c':
+		// Probably gonna remove this, pretty useless and bad overall for security
+			system("clear");
+        	system("figlet \"jC.CMD\"");
+        	cout << "~> ";
+        	getline(cin, jccmd);
+        	system(jccmd.c_str());
+			break;
+		case 'p':
+		// Exits JMLT and runs pacmany
+			system("clear");
+        	system("pacmany"); 
+        	return 0;
+			break;
+		case 'y':
+		// Exits JMLT and runs yayy
+			system("clear");
+        	system("yayy"); 
+        	return 0;
+			break;
+		case 'q':
+		// Exits JMLT
+			return 0;
+			break;
+		default:
+		// Goes to menu() if user enters an invalid input
+        	system("clear");
+        	cout << "Unknown option, try again.\n"; 
+        	main();
+			break;
+	}
 }
